b3cdad32015dc3c9d639c37cd0bea0754f239f23
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-14 20:11:13 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,21 +1,11 @@
 def reformat_languages(languages)
-  new_hash = {}
-  languages.each do |style_char, language|
-    #puts style_char
-    language.each do |type, language_type|
-      language_type.each do |attribute, value|
-        if new_hash[type].nil?
-          new_hash[type] = {}
-        end
-        new_hash[type][:style] ||= []
-        new_hash[type][:style] << style_char
-      #new_hash[type] = language_type
-      #{style: style_char}
-      if new_hash[type][attribute].nil?
-          new_hash[type][attribute] = value
-        end
-     end
+  new_languages = {}
+  languages.each do |type, hash|
+    hash.each do |name, attributes|
+      new_languages[name] ||= attributes
+      new_languages[name][:style] ||= []
+      new_languages[name][:style] << type
     end
   end
-  return new_hash
+  new_languages
 end
